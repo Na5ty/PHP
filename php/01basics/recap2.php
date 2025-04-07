6ae66289d2c5af4874db34975074edb3a4497d7d
Na5ty <thomas.zeides@web.de>
2025-04-07 12:02:03 +0200

    recap monday 07.04.2025

---
+++ php/01basics/recap2.php
@@ -0,0 +1,54 @@
+<?php
+
+    //print every number from 1 to 15
+    for ($i = 01; $i <= 15; $i++){
+        echo $i . "\n";
+    }
+
+    echo "<br>";
+
+    //print every odd number from 1 to 20
+    $num = 1;
+
+    while($num < 20){
+        if($num % 2 !== 0){
+            echo $num . "is a odd number <br>";
+        } 
+        $num ++;
+    }
+
+
+    //countdown 10 to 1
+    $num1 = 10;
+
+    do {
+        echo $num1 . "<br>";
+        $num1--;
+    } while ($num1 >= 1);
+
+
+    //loop through an array
+    $colors = array("red", "green", "blue", "yellow");
+    foreach ($colors as $color){
+        echo $color . "\n";
+    }
+
+    //print all multiples of 5
+    for($n = 1; $n <= 10; $n++){
+        echo $n * 5 . "\n";
+    }
+
+    echo "<br>";
+    //print the sum of numbers 1 to 100
+    for($m = 1; $m <= 100; $m++){
+        $sum += $m;
+    }
+    echo $sum . "<br>";
+
+    //associative arrays
+    $name = array ("name" => "Alice", "age" => 25, "city" => "Paris");
+
+    foreach($name as $x => $y){
+        echo "$x: $y <br>";
+    }
+?>
